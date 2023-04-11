15c37daa2cf1668a6b683f5794195e1a24729535
Elizaveta Buibarova <liza.buybarova@gmail.com>
2023-04-11 23:29:26 +0300

    Task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+
+
+function onSubm(event) {
+  event.preventDefault();
+  const startDelay = Number(form.elements.delay.value);  
+  const step = Number(form.elements.step.value);  
+  const amount = Number(form.elements.amount.value);
+  let delay = startDelay;
+
+  if (amount <= 0) {
+    Notify.info("Enter plus amount");
+    return;
   }
+  
+
+  for (let i = 1; i <= amount; i += 1) {       
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+      Notify.info(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+      .catch(({ position, delay }) => {
+      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+    delay += step;    
+  }
+}
+
+form.addEventListener('submit', onSubm);
+
+function createPromise(position, delay) {
+  const canFulfill = Math.random() > 0.3;
+
+  return new Promise((resolve, reject) => {
+   setTimeout(() => {
+      if (canFulfill) {
+        resolve({position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  })
 }
