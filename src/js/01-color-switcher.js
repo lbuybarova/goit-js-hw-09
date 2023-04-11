7d2eaa6ea2024298be39cd5e0b0c0be9b8e15651
Elizaveta Buibarova <liza.buybarova@gmail.com>
2023-04-11 23:11:39 +0300

    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const DELAY = 1000;
+const refs = {
+    start: document.querySelector("[data-start]"),
+    stop: document.querySelector("[data-stop]"),
+    body: document.body
+}
 
+refs.stop.disabled = true;
+
+const start = document.querySelector("[data-start]");
+
+let interval = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.start.addEventListener('click', onStart);
+
+function onStart() {
+    refs.start.disabled = true;
+    refs.stop.disabled = false;
+    interval = setInterval(() => {
+       refs.body.style.backgroundColor = getRandomHexColor(); 
+    }, DELAY);    
+}
+
+refs.stop.addEventListener('click', onStop);
+
+function onStop() {
+    refs.start.disabled = false;
+    refs.stop.disabled = true;
+    clearInterval(interval);
+} 
